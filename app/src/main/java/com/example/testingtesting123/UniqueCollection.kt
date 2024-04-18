21eabd0af4293b0c4cfb43ba99d13ee4fb21cecc
Gavin Mai <tul33743@temple.edu>
2024-04-18 12:09:09 -0400

    Tests for getItem, remove index0, remove middle, remove last

--- app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
+++ app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
@@ -19,6 +19,4 @@ class UniqueCollection {
     }
 
     fun size() = collection.size
-
-
 }
--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -30,15 +30,77 @@ class UniqueCollectionTest {
         assert (collection.size() == 2)
     }
 
+//    @Test
+//    fun clearAllItems() {
+//        collection.addItem(Item("item1"))
+//        collection.addItem(Item("Item2"))
+//
+//        val originalSize = collection.size()
+//        collection.clear()
+//        val newSize = collection.size();
+//
+//        assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
+//    }
+
     @Test
-    fun clearAllItems() {
-        collection.addItem(Item("item1"))
-        collection.addItem(Item("Item2"))
+    fun getItem(){
+        collection.addItem(Item("1"))
+        collection.addItem(Item("2"))
 
-        val originalSize = collection.size()
-        collection.clear()
-        val newSize = collection.size();
+        val item1 = collection.get(0)
+        val item2 = collection.get(1)
 
-        assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
+        assert(item1.name == "1" && item2.name == "2"){"Get does not work properly"}
     }
+
+    @Test
+    fun removeItemSize(){
+        repeat(10){
+            collection.addItem(Item(it.toString()))
+        }
+
+        val oldSize = collection.size()
+        collection.remove(Item("1"))
+        val newSize = collection.size()
+        assert(newSize == oldSize-1){"Remove did not change collection size"}
+    }
+
+    @Test
+    fun removeItemIndex0(){
+        repeat(10){
+            collection.addItem(Item(it.toString()))
+        }
+
+        collection.remove(Item("1"))
+        val item0 = collection.get(1)
+        assert(item0.name == "2"){"Remove did not remove item0:"}
+    }
+
+    @Test
+    fun removeItemEmptyCollection(){
+        collection.remove(Item("1"))
+        assert(true){"Remove on an empty list"}
+    }
+
+    @Test
+    fun removeItemIndexMiddle(){
+        repeat(10){
+            collection.addItem(Item(it.toString()))
+        }
+        collection.remove(Item("5"))
+        val item0 = collection.get(5)
+        assert(item0.name == "6"){"Remove did not remove item at middle"}
+    }
+
+    @Test
+    fun removeItemIndexLast(){
+        repeat(10){
+            collection.addItem(Item(it.toString()))
+        }
+
+        collection.remove(Item("10"))
+        val item0 = collection.get(collection.size()-1)
+        assert(item0.name == "9"){"Remove did not remove item at end"}
+    }
+
 }
